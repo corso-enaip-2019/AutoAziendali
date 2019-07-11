63dc5f7d6d20c976925b758450f6dc5291989336
TeamAuto <f.tonizzo@libero.it>
2019-07-11 12:20:04 +0200

    pagine stato utilizzo

--- AutoAziendaliLocale/AutoAziendali/AngCarApp/src/app/models/StatoVeicolo.ts
+++ AutoAziendaliLocale/AutoAziendali/AngCarApp/src/app/models/StatoVeicolo.ts
@@ -5,7 +5,7 @@ export class StatoVeicolo
     constructor(public IdStatoVeicolo: number, public IdVeicolo: number, public IdProprieta: number, public IdLibretto: number,
         public IdTelepass: number, public IdViacard: number, public Data: Date, public IdStato: number,
         public ChilometriRental: number, public IdSocieta: number, public IdBusinessUnit: number,
-        public IdModalita: number, public IdAnagrafica: string){
+        public IdModalita: number, public IdAnagrafica: string,public Note: string){
 
     }
 
